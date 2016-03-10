e86d8802940ab61e57a6c4b6a47fde785af28c5f
Saad <sqfmason@yahoo.com>
2016-03-10 09:46:20 -0600

    added users controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+	before_action :set_user, only:[:show]
+
+
+def index
+    	@users = User.all
+    end
+    
+  def show
+  end
+
+   private
+
+  def set_user
+  	@user = User.find_by(username: params[:id])
+  end
+
+end
