98775927291c04291997431af1771c346056d29a
Saad <sqfmason@yahoo.com>
2016-03-10 11:22:10 -0600

    Added posts controller, views and routes

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 gem 'simple_form'
 gem 'devise'
 gem 'gravatarify', '~> 3.0.0'
+gem 'public_activity'
+gem 'auto_html'
--- db/migrate/20160310165331_create_friendships.rb
+++ db/migrate/20160310165331_create_friendships.rb
@@ -1,9 +1,10 @@
+
 class CreateFriendships < ActiveRecord::Migration
   def change
+  	
     create_table :friendships do |t|
       t.integer :user_id
       t.integer :friend_id
-      t.string :state
       t.string   :state, default: "pending"
       t.datetime :friended_at
 
